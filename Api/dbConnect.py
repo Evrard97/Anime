9119d73846c81541fbbc46527d83de8919a7227f
Evrard97 <Aka-evrard.aka@outlook.com>
2023-03-01 18:56:46 +0100

    update api

--- Api/dbConnect.py
+++ Api/dbConnect.py
@@ -11,13 +11,13 @@ load_dotenv()
 #pip install bson
 # Connexion à la base de données
 def connectDB() :
-    client = MongoClient('localhost', 27017)
-    db_name = client['Anime']
-    # uri = "mongodb+srv://"+os.getenv('NAME')+":"+os.getenv('PASSWORD')+"@rudybanger.gevfxpa.mongodb.net/Projet?retryWrites=true&w=majority"
-    # client = pymongo.MongoClient(uri)
+    # client = MongoClient('localhost', 27017)
+    # db_name = client['Anime']
+    uri = "mongodb+srv://"+os.getenv('NAME')+":"+os.getenv('PASSWORD')+"@rudybanger.gevfxpa.mongodb.net/Projet?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(uri)
     # Remplacez le nom de la collection et de la colonne par les vôtres
-    # db = client.Projet
-    # collection = db.Anime
-    db = db_name.anime
-    return db
+    db = client.Projet
+    collection = db.Anime
+    # db = db_name.anime
+    return collection
 
